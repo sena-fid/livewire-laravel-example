a3c38e113c77cda2bcb3baa954828c1c5bbe35e5
sena-fidö <senafdn@icloud.com>
2022-10-28 15:27:52 +0300

    @livewireScripts

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -6,6 +6,18 @@ use Livewire\Component;
 
 class Counter extends Component
 {
+    public $number = 0;
+
+    public function increment(){
+
+        $this->number++;
+    }
+
+    public function decrement(){
+        
+        $this->number--;
+    }
+
     public function render()
     {
         return view('livewire.counter');
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -3,7 +3,15 @@
         <div class="row">
             <div class="col-lg-8 offset-lg-2">
                 <div class="card card-body">
-                    hello
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div>
+                            <button class="btn btn-primary" wire:click="decrement">-</button>
+                        </div>
+                        <h2>{{ $number }}</h2>
+                        <div>   
+                            <button class="btn btn-primary" wire:click="increment">+</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
