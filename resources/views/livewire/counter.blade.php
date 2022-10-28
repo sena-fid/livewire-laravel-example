20ba77f9e6231a97266cf68f08300e5d54279c3e
sena-fidö <senafdn@icloud.com>
2022-10-28 15:16:10 +0300

    include livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,11 @@
+<div class="mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="card card-body">
+                    hello
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
